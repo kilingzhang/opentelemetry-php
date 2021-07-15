ecef42475f8846ea2643aa25e70586a84ca952ce
kilingzhang <i@kilingzhang.com>
2021-07-15 11:59:35 +0800

    + trace 获取 trace 埋点 抽样判断

--- src/OpenTelemetry.php
+++ src/OpenTelemetry.php
@@ -87,6 +87,15 @@ class OpenTelemetry
     /**
      * @return string
      */
+    public static function getSampledTraceParent()
+    {
+        list($version, $traceId, $spanId, $flag) = self::parseTraceParent();
+        return "$version-$traceId-$spanId-01";
+    }
+
+    /**
+     * @return string
+     */
     public static function getTraceState()
     {
         if (function_exists('opentelemetry_get_tracestate')) {
@@ -96,14 +105,102 @@ class OpenTelemetry
     }
 
     /**
+     * @return array
+     */
+    public static function parseTraceStates()
+    {
+        $traceStates = [];
+        $traceState = self::getTraceState();
+        $traceState = explode(',', $traceState);
+        foreach ($traceState as $item) {
+            list($k, $v) = explode('=', $item);
+            $traceStates[$k] = $v;
+        }
+        return $traceStates;
+    }
+
+    /**
+     * @param array $states
+     * @return string
+     */
+    public static function getTraceStateWithValues($states = [])
+    {
+        $traceState = '';
+        $traceStates = self::parseTraceStates();
+        $traceStates = array_merge($traceStates, $states);
+        foreach ($traceStates as $k => $v) {
+            $traceState = "$k=$v,";
+        }
+        return trim($traceState, ',');
+    }
+
+    /**
+     * @param $key
+     * @return string
+     */
+    public static function getTraceStateValue($key)
+    {
+        $traceStates = self::parseTraceStates();
+        return empty($traceStates[$key]) ? '' : $traceStates[$key];
+    }
+
+
+    /**
+     * version trace 版本 w3c 协议 默认 00
+     * trace id
+     * span id
+     * flag 是否收集trace记录 00 不收集 01 收集
+     * @return array
+     */
+    public static function parseTraceParent($traceParent = '')
+    {
+        empty($traceParent) && $traceParent = self::getTraceParent();
+        $version = '00';
+        $traceId = '';
+        $spanId = '';
+        $flag = '00';
+        $parentTrace = self::getTraceParent();
+        $parentTrace = explode('-', $parentTrace);
+        if (!empty($parentTrace) && count($parentTrace) == 4) {
+            list($version, $traceId, $spanId, $flag) = $parentTrace;
+        }
+        return [$version, $traceId, $spanId, $flag];
+    }
+
+    /**
      * @return string
      */
     public static function getTraceId()
     {
-        if (function_exists('opentelemetry_get_trace_id')) {
-            return opentelemetry_get_trace_id();
-        }
-        return '';
+        list($version, $traceId, $spanId, $flag) = self::parseTraceParent();
+        return $traceId;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getSpanId()
+    {
+        list($version, $traceId, $spanId, $flag) = self::parseTraceParent();
+        return $spanId;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getVersion()
+    {
+        list($version, $traceId, $spanId, $flag) = self::parseTraceParent();
+        return $version;
+    }
+
+    /**
+     * @return string
+     */
+    public static function isSampled()
+    {
+        list($version, $traceId, $spanId, $flag) = self::parseTraceParent();
+        return $flag == '01';
     }
 
     /**
