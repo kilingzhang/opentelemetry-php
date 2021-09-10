29c0556011026a2e7af848a1e230373724074289
kilingzhang <i@kilingzhang.com>
2021-09-10 10:14:08 +0800

    更新日志规范

--- src/Log.php
+++ src/Log.php
@@ -9,7 +9,6 @@ class Log
 
     const EMERGENCY = 'emergency';
     const ALERT = 'alert';
-    const CRITICAL = 'critical';
     const ERROR = 'error';
     const WARNING = 'warning';
     const NOTICE = 'notice';
@@ -17,7 +16,7 @@ class Log
     const DEBUG = 'debug';
 
     /**
-     * 可任意级别记录日志。
+     * 可任意级别记录日志。 禁止外部直接调用
      *
      * @param mixed $level
      * @param string $message
@@ -50,7 +49,9 @@ class Log
     }
 
     /**
-     * 系统无法使用。
+     * 必须立即采取行动。
+     * 系统无法使用。例如：fatal,panic,服务不可用,资源不可达
+     * 立即触发告警 告警通道（短信,IVR,邮件等）默认且必须立刻告警，无需业务自行订阅。
      *
      * @param string $message
      * @param array $context
@@ -63,10 +64,8 @@ class Log
     }
 
     /**
-     * 必须立即采取行动。
-     *
-     * 例如: 整个网站宕机了，数据库挂了，等等。 这应该
-     * 发送短信通知警告你.
+     * 业务层面的需要进行告警通知的日志。例如: 红包余额不足,账户余额不足等
+     * 需要业务自己根据告警信息进行订阅告警。
      *
      * @param string $message
      * @param array $context
@@ -78,25 +77,11 @@ class Log
         Tracer::addAttribute("logging.alert.message", $message);
     }
 
-    /**
-     * 临界条件。
-     *
-     * 例如: 应用组件不可用，意外的异常。
-     *
-     * @param string $message
-     * @param array $context
-     * @return void
-     */
-    public static function critical($message, array $context = array())
-    {
-        self::log(self::CRITICAL, $message, $context);
-        Tracer::addAttribute("logging.critical.message", $message);
-    }
 
     /**
      * 运行时错误不需要马上处理，
      * 但通常应该被记录和监控。
-     *
+     * 支付失败、权益下发失败
      * @param string $message
      * @param array $context
      * @return void
@@ -111,6 +96,7 @@ class Log
      * 例外事件不是错误。
      *
      * 例如: 使用过时的API，API使用不当，不合理的东西不一定是错误。
+     * 接口请求业务码不符合预期、方法参数不正确
      *
      * @param string $message
      * @param array $context
@@ -123,7 +109,7 @@ class Log
 
     /**
      * 正常但重要的事件.
-     *
+     * 例如: 权益,支付
      * @param string $message
      * @param array $context
      * @return void
@@ -135,8 +121,8 @@ class Log
 
     /**
      * 有趣的事件.
-     *
-     * 例如: 用户登录，SQL日志。
+     * 可用于记录事件节点。标记事件发生。
+     * 例如: 用户登录,SQL日志。
      *
      * @param string $message
      * @param array $context
@@ -149,7 +135,7 @@ class Log
 
     /**
      * 详细的调试信息。
-     *
+     * 例如: 接口的请求及响应,方法的输入输出
      * @param string $message
      * @param array $context
      * @return void
